cdbd2b3b31a668e880cca1bbdef1d62f214844c2
Mario Todorov <mario.yankov.todorov@gmail.com>
2021-04-22 12:28:14 +0300

    Modified files

--- 001.b_to_gate_level/1.design.v
+++ 001.b_to_gate_level/1.design.v
@@ -4,17 +4,17 @@
 `timescale 1ns / 1ps
 module 	b_to_gray_gate_level 
       (
-		input 	b3,
-		input 	b2,
+        input 	b3,
+        input 	b2,
         input 	b1,
-		input 	b0,
-		output 	g3,
+        input 	b0,
+        output 	g3,
         output	g2,
         output	g1,
         output  g0
        );
   
-  	
+      
   buf(g3, b3);
   
   xor(g2, b3, b2);
--- 001.b_to_gate_level/2.testbench.sv
+++ 001.b_to_gate_level/2.testbench.sv
@@ -16,123 +16,123 @@ module b_to_gray_gate_level_TB;
 	// Outputs
 	wire g3;
   	wire g2;
-    wire g1;
+	wire g1;
   	wire g0;
  
   
 	// Instantiate the Unit Under Test (UUT)
 	b_to_gray_gate_level uut (
-          .b3(b3),
-          .b2(b2),
-          .b1(b1),
-          .b0(b0),
-          .g3(g3),
-          .g2(g2),
-          .g1(g1),
-          .g0(g0)
+		  .b3(b3),
+		  .b2(b2),
+		  .b1(b1),
+		  .b0(b0),
+		  .g3(g3),
+		  .g2(g2),
+		  .g1(g1),
+		  .g0(g0)
 	);
 
 
 	initial begin
-      
-      	$dumpfile("dump.vcd");
-        $dumpvars(1,     b_to_gray_gate_level_TB);
-        
+	  
+	  	$dumpfile("dump.vcd");
+		$dumpvars(1,     b_to_gray_gate_level_TB);
+		
 		$display("b_to_gray_gate_level_TB");
 
 	$monitor 
-      (
-       "%t\|  binary --> %b %b %b %b -- %b %b %b %b <-- gray", $time, b3, b2, b1, b0, g3, g2, g1, g0
-      );
+	  (
+	   "%t\|  binary --> %b %b %b %b -- %b %b %b %b <-- gray", $time, b3, b2, b1, b0, g3, g2, g1, g0
+	  );
 		
 		// Initialize Inputs
-      		// 0
+	  		// 0
 			b3 = 0;
 			b2 = 0;
-      		b1 = 0;
+	  		b1 = 0;
 			b0 = 0;
 
 
 		// Add stimulus here
-        #10 // 1
-        	b3 = 0;
+		#10 // 1
+			b3 = 0;
 			b2 = 0;
-      		b1 = 0;
+	  		b1 = 0;
 			b0 = 1;
-        #10 // 2
-        	b3 = 0;
+		#10 // 2
+			b3 = 0;
 			b2 = 0;
-      		b1 = 1;
+	  		b1 = 1;
 			b0 = 0;
-        #10 // 3
-        	b3 = 0;
+		#10 // 3
+			b3 = 0;
 			b2 = 0;
-      		b1 = 1;
+	  		b1 = 1;
 			b0 = 1;
-        #10 // 4
-        	b3 = 0;
+		#10 // 4
+			b3 = 0;
 			b2 = 1;
-      		b1 = 0;
+	  		b1 = 0;
 			b0 = 0;
-        #10 // 5
-        	b3 = 0;
+		#10 // 5
+			b3 = 0;
 			b2 = 1;
-      		b1 = 0;
+	  		b1 = 0;
 			b0 = 1;
-        #10 // 6
-        	b3 = 0;
+		#10 // 6
+			b3 = 0;
 			b2 = 1;
-      		b1 = 1;
+	  		b1 = 1;
 			b0 = 0;
-        #10 // 7
-        	b3 = 0;
+		#10 // 7
+			b3 = 0;
 			b2 = 1;
-      		b1 = 1;
+	  		b1 = 1;
 			b0 = 1;
-        #10 // 8
-        	b3 = 1;
+		#10 // 8
+			b3 = 1;
 			b2 = 0;
-      		b1 = 0;
+	  		b1 = 0;
 			b0 = 0;
-        #10 // 9
-        	b3 = 1;
+		#10 // 9
+			b3 = 1;
 			b2 = 0;
-      		b1 = 0;
+	  		b1 = 0;
 			b0 = 1;
-        #10 // 10
-        	b3 = 1;
+		#10 // 10
+			b3 = 1;
 			b2 = 0;
-      		b1 = 1;
+	  		b1 = 1;
 			b0 = 0;
-        #10 // 11
-        	b3 = 1;
+		#10 // 11
+			b3 = 1;
 			b2 = 0;
-      		b1 = 1;
+	  		b1 = 1;
 			b0 = 1;
-        #10 // 12
-        	b3 = 1;
+		#10 // 12
+			b3 = 1;
 			b2 = 1;
-      		b1 = 0;
+	  		b1 = 0;
 			b0 = 0;
-        #10 // 13
-        	b3 = 1;
+		#10 // 13
+			b3 = 1;
 			b2 = 1;
-      		b1 = 0;
+	  		b1 = 0;
 			b0 = 1;
-        #10 // 14
-        	b3 = 1;
+		#10 // 14
+			b3 = 1;
 			b2 = 1;
-      		b1 = 1;
+	  		b1 = 1;
 			b0 = 0;
-        #10 // 15
-        	b3 = 1;
+		#10 // 15
+			b3 = 1;
 			b2 = 1;
-      		b1 = 1;
+	  		b1 = 1;
 			b0 = 1;
 
 		
 		#20 $finish;
 
 	end
-      
+
 endmodule
