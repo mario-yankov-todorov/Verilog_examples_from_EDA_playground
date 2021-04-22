8290a8a3785bb729d2c5709900d92c73b962911c
Mario Todorov <mario.yankov.todorov@gmail.com>
2021-04-22 12:21:39 +0300

    Modified files

--- 001.b_to_gate_level/1.design.v
+++ 001.b_to_gate_level/1.design.v
@@ -1,16 +1,13 @@
 /*
 *	M. Todorov 2021
 */
-
 `timescale 1ns / 1ps
-
 module 	b_to_gray_gate_level 
       (
 		input 	b3,
-	    input 	b2,
+		input 	b2,
         input 	b1,
 		input 	b0,
-
 		output 	g3,
         output	g2,
         output	g1,
@@ -23,6 +20,4 @@ module 	b_to_gray_gate_level
   xor(g2, b3, b2);
   xor(g1, b2, b1);
   xor(g0, b1, b0);
-
-
 endmodule
